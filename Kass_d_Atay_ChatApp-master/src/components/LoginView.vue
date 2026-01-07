670200abcc2a3cb08f676fb04dba3921fa8acc68
farahbouhzam <farah.bouzam@um6p.ma>
2026-01-07 23:01:58 +0100

    commit

--- Kass_d_Atay_ChatApp-master/src/components/LoginView.vue
+++
@@ -1,8 +0,0 @@
-<!-- src/views/LoginView.vue -->
-<template>
-    <Login />
-  </template>
-  
-  <script setup>
-  import Login from '@/components/LoginPage.vue';
-  </script>
--- Kass_d_Atay_ChatApp-master/src/components/SignUpView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <SignUp />
-  </template>
-  
-  <script setup>
-  import SignUp from '@/components/RegisterPage.vue';
-  </script>
